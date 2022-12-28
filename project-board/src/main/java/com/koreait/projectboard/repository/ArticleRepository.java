ef75e02a0d22ec4905d648d2b3d3bbe1d8d423ca
Sophie <sophiakim.dev@gmail.com>
2022-12-28 11:47:19 +0900

    #8 - repository interface 작성
    db애 접근하기 위해 작성

---
+++ project-board/src/main/java/com/koreait/projectboard/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.koreait.projectboard.repository;
+
+import com.koreait.projectboard.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+
+}
